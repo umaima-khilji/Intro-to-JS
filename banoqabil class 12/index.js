ac7b7b6b8426e1e88bb8fd3580044e7901020703
Umaima Khilji <91422673+umaima-khilji@users.noreply.github.com>
2023-04-13 15:19:24 +0500

    Add files via upload

---
+++ banoqabil class 12/index.js
@@ -0,0 +1,99 @@
+// var a = 3
+// console.log(a+2)
+
+//COmditions 
+// if(){
+    
+// }
+//for value assign =
+// var biscuits = "zera plus"
+
+//checks value ==
+// if(iscuits = "zera plus"){
+//     alert('dedo')
+// }
+
+// else if(biscuits = "sooper"){
+//     alert('2 half eoll dedo')
+// }
+
+// else{
+//     alert('khtm karo')
+// }
+
+////
+// console.log(biscuits == "sooper")
+
+///////
+// var myage = '21'
+// console.log(myage> 21)
+
+////
+// var city = prompt('where do you live?')
+
+
+// if(city == karachi){
+//     var area = +prompt(how near you are from mazar e quaid)
+
+//     if(area<20){
+//         console.log('your center is idara')
+//     }
+//     else{
+//         console.log('your center is escula')
+//     }
+
+// }
+
+// else{
+//     console.log('you are not eligible')
+// }
+
+//////////////////////////
+// var area = +prompt('how near you are from mazar e quaid')
+// if((city == 'krachi' || city == 'karachi') && (area < 20)){
+//     console.log('your center is idara')
+// } 
+
+// else{
+//     console.log('you are not eligible')
+// }
+
+////////////////////////////
+//Array
+// var students = ['ifra', 'sawaira' ,'umaima', 'neha', 'tani partner']
+// students[0] = 'rabi'
+// students[3] = 'umaima'
+// students[6] = 'neha'
+//push will add in last, pop remove from last
+// students.push('umaima')
+// students.pop()
+//var out = students.pop()
+// var out = students.shift()
+// students.unshift('umaima' , 'sawaira')
+
+//0 for not deleting the name,  1 for deleting the name
+// students.splice(2,0 , 'faiza')
+// students.splice(2,1 , 'faiza')
+
+//if you want to make copy of arry kahn sy lyky kahn tak arry py effect nhi hoga
+// var std = students.slice(1,4)
+
+// console.log(students[0])
+// console.log(students[1])
+// console.log(students[2])
+// console.log(students[3])
+// console.log(students[4])
+
+
+// console.log(students)
+// console.log(std)
+
+//for loop 4
+
+// for(var i = 0; i < students.length; i++){
+// console.log(i)
+// }
+
+
+
+//use slice and splice for capitalization 
