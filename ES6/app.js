b9ebca656395d8a81e29d67740d15f8bdda3c7ca
Umaima Khilji <91422673+umaima-khilji@users.noreply.github.com>
2023-05-30 17:24:55 +0500

    Add files via upload

---
+++ ES6/app.js
@@ -0,0 +1,88 @@
+// //var , let , const
+// //var global scoping
+// //let and const block scoping srif function ky andr acces kar skty hain
+// //es5 : vanila javascript
+
+// // var myVar = 10;
+// // var myVar = 10;
+
+// //ES6:
+// // let myVar = 10;
+// //app reassign kar skty hain re declear nhi kar skty hain 
+
+// //let ko aap value assign kar skty ho , reassign kar skty hain
+// //const ko nhi kar skty
+// // const PI = 3.12;
+// //reassign bhi nhi kar skty 
+
+
+// //hoisting in js: function bana hua hai es5 or es6 ka toh es5 ka chaly ga 
+// //q k return baad main kr rhy hain
+// //phly bany ga phir call hoga : is hoisting
+
+// //Functions in es6:
+
+// // arrow function:
+// const fun = () => {
+// console.log("chal rah hai")
+
+// //closure property:
+// const anotherfunc = () => {
+//     console.log()
+// }
+// anotherfunc();
+// //function ky andr ek or func bana kar main func main call kara closuer
+// //yeh isi main hi call hoga isky bar nhi hoga
+// }
+//  fun();
+
+// ////////////
+// // ek function ly rhy hain us main jtna time dyna hai laga lo 
+// //set timer
+// setTimeout(fun, 3000)
+
+//callback function
+// setTimeout() => {
+//     console.log("chal rah hai")
+// }
+//func as a perameter use hot HAI DOSTRY FUNCTION MAIN.
+//ek fun ko as a peramiter function or do is called call back fun
+//////
+//IIEF
+//bagair name ky function banaya
+//sab sy zaida prority aap ko anpy function main dyni hoga
+//esa func jo direct chal jae
+
+
+// let myName = "umaima"
+// let mydusman = "ifra"
+
+// console.log("my name is :" + "" + myName + "" + "my dusman is" + mydusman)
+// console.log("my name is: ${myName}  " )
+
+// //ternary operator
+// //if else:
+// myName == 'umaima' ? (console.log('do this')) : (console.log('do this'))
+// //if is ?
+//else is :
+//else if:
+// myName !== 'umaima' ?
+//  (console.log('do this')) 
+//  :
+//  mydusman == 'sawaira' ? (console.log('heyyyy'))
+
+
+//object
+// let myObj ={
+//     name : 'umaima',
+//     age: 21
+// }
+// const { name, age} = myObj
+// console.log(name,age)
+
+///////
+//5000: 5 sec
+// setTimeout(() => console.log("hello"), 5000)
+
+
+
